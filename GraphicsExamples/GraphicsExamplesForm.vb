f726da2db212f1a2d0bc535571efdd77dc9bfdfe
Noah Holloway <noahholloway23@gmail.com>
2025-03-31 08:51:20 -0600

    started draw rectangle

--- GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamplesForm.vb
@@ -9,14 +9,34 @@ Public Class GraphicsExamplesForm
         Dim g As Graphics = Me.CreateGraphics
         Dim pen As New Pen(Color.Black)
 
-        'pen.Color = Color.Black
+        ' pen.Color = Color.Black
         g.DrawLine(pen, 50, 50, 100, 100)
+        g.Dispose()
+    End Sub
+
+    Sub DrawRectangle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red, 3)
+
+        ' pen.Color = Color.Black
+        g.DrawRectangle(pen, 150, 150, 300, 300)
+        g.Dispose()
+    End Sub
+
+    Sub DrawEllipse()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.DarkSalmon, 3)
+
+        ' pen.Color = Color.Black
+        g.DrawEllipse(pen, 160, 160, 290, 240)
         g.Dispose()
     End Sub
 
     'event handlers*****************************************************************************
     Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         DrawLine()
+        DrawRectangle()
+        DrawEllipse()
     End Sub
 
     Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
