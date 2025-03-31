21924b4ccdd70c8a28890affe9cfb246759e3173
Noah Holloway <noahholloway23@gmail.com>
2025-03-31 08:30:08 -0600

    added mouse move tracking

--- GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamplesForm.vb
@@ -1,5 +1,25 @@
-﻿Public Class GraphicsExamplesForm
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿'Noah Holloway
+'RCET 2265
+'Spring 2025
 
+Option Strict On
+Option Explicit On
+Public Class GraphicsExamplesForm
+    Sub DrawLine()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+
+        'pen.Color = Color.Black
+        g.DrawLine(pen, 50, 50, 100, 100)
+        g.Dispose()
+    End Sub
+
+    'event handlers*****************************************************************************
+    Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        DrawLine()
+    End Sub
+
+    Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Me.Text = $"({e.X},{e.Y})"
     End Sub
 End Class
