44993a8ebd75b1c1d2d742bba738d8b278fa95d0
Noah Holloway <noahholloway23@gmail.com>
2025-04-01 08:18:19 -0600

    add foregroundcolor function

--- GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamplesForm.vb
@@ -5,11 +5,20 @@
 Option Strict On
 Option Explicit On
 Public Class GraphicsExamplesForm
+    Function ForeGroundColor(Optional newColor As Color = Nothing) As Color
+        Static _foreColor As Color = Color.Black
+
+        If newColor <> Nothing Then
+            _foreColor = newColor
+        End If
+        Return _foreColor
+
+    End Function
     Sub DrawLine()
         Dim g As Graphics = Me.CreateGraphics
         Dim pen As New Pen(Color.Black)
 
-        ' pen.Color = Color.Black
+        pen.Color = ForeGroundColor(Color.Lime)
         g.DrawLine(pen, 50, 50, 100, 100)
         g.Dispose()
     End Sub
@@ -32,11 +41,25 @@ Public Class GraphicsExamplesForm
         g.Dispose()
     End Sub
 
+    Sub DrawString()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red, 3)
+        ' Create font and brush.
+        Dim drawFont As New Font("Arial", 16)
+        Dim drawBrush As New SolidBrush(Color.Crimson)
+
+
+        g.DrawString("Graphics!", drawFont, drawBrush, 200, 200)
+
+        g.Dispose()
+    End Sub
+
     'event handlers*****************************************************************************
     Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         DrawLine()
         DrawRectangle()
         DrawEllipse()
+        DrawString()
     End Sub
 
     Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
