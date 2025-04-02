687c41b14b8a1cbb08824f782547e21bd8d5d975
Noah Holloway <noahholloway23@gmail.com>
2025-04-02 08:35:51 -0600

    added foreground color change

--- GraphicsExamples/GraphicsExamplesForm.Designer.vb
+++ GraphicsExamples/GraphicsExamplesForm.Designer.vb
@@ -22,13 +22,21 @@ Partial Class GraphicsExamplesForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.ColorDialog = New System.Windows.Forms.ColorDialog()
         Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ForegroundColorTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MainContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ForegroundColorContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundColorContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WidthContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearContextMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TopMenuStrip.SuspendLayout()
+        Me.MainContextMenuStrip.SuspendLayout()
         Me.SuspendLayout()
         '
         'TopMenuStrip
@@ -66,17 +74,56 @@ Partial Class GraphicsExamplesForm
         Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
         Me.HelpToolStripMenuItem.Text = "&Help"
         '
+        'MainContextMenuStrip
+        '
+        Me.MainContextMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MainContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ForegroundColorContextMenuItem, Me.BackgroundColorContextMenuItem, Me.WidthContextMenuItem, Me.FontContextMenuItem, Me.ClearContextMenuItem})
+        Me.MainContextMenuStrip.Name = "MainContextMenuStrip"
+        Me.MainContextMenuStrip.Size = New System.Drawing.Size(211, 152)
+        '
+        'ForegroundColorContextMenuItem
+        '
+        Me.ForegroundColorContextMenuItem.Name = "ForegroundColorContextMenuItem"
+        Me.ForegroundColorContextMenuItem.Size = New System.Drawing.Size(210, 24)
+        Me.ForegroundColorContextMenuItem.Text = "Foreground Color"
+        '
+        'BackgroundColorContextMenuItem
+        '
+        Me.BackgroundColorContextMenuItem.Name = "BackgroundColorContextMenuItem"
+        Me.BackgroundColorContextMenuItem.Size = New System.Drawing.Size(210, 24)
+        Me.BackgroundColorContextMenuItem.Text = "Background Color"
+        '
+        'WidthContextMenuItem
+        '
+        Me.WidthContextMenuItem.Name = "WidthContextMenuItem"
+        Me.WidthContextMenuItem.Size = New System.Drawing.Size(210, 24)
+        Me.WidthContextMenuItem.Text = "Width"
+        '
+        'FontContextMenuItem
+        '
+        Me.FontContextMenuItem.Name = "FontContextMenuItem"
+        Me.FontContextMenuItem.Size = New System.Drawing.Size(210, 24)
+        Me.FontContextMenuItem.Text = "Font"
+        '
+        'ClearContextMenuItem
+        '
+        Me.ClearContextMenuItem.Name = "ClearContextMenuItem"
+        Me.ClearContextMenuItem.Size = New System.Drawing.Size(210, 24)
+        Me.ClearContextMenuItem.Text = "Clear"
+        '
         'GraphicsExamplesForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ContextMenuStrip = Me.MainContextMenuStrip
         Me.Controls.Add(Me.TopMenuStrip)
         Me.MainMenuStrip = Me.TopMenuStrip
         Me.Name = "GraphicsExamplesForm"
         Me.Text = "Form1"
         Me.TopMenuStrip.ResumeLayout(False)
         Me.TopMenuStrip.PerformLayout()
+        Me.MainContextMenuStrip.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -88,4 +135,10 @@ Partial Class GraphicsExamplesForm
     Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ForegroundColorTopMenuItem As ToolStripMenuItem
+    Friend WithEvents MainContextMenuStrip As ContextMenuStrip
+    Friend WithEvents ForegroundColorContextMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundColorContextMenuItem As ToolStripMenuItem
+    Friend WithEvents WidthContextMenuItem As ToolStripMenuItem
+    Friend WithEvents FontContextMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearContextMenuItem As ToolStripMenuItem
 End Class
--- GraphicsExamples/GraphicsExamplesForm.vb
+++ GraphicsExamples/GraphicsExamplesForm.vb
@@ -17,7 +17,7 @@ Public Class GraphicsExamplesForm
 
     Sub DrawWithMouse(oldX As Integer, oldY As Integer, newX As Integer, newY As Integer)
         Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(ForeColor)
+        Dim pen As New Pen(ForeGroundColor)
 
 
         g.DrawLine(pen, oldX, oldY, newX, newY)
@@ -64,13 +64,13 @@ Public Class GraphicsExamplesForm
     End Sub
 
     'event handlers*****************************************************************************
-    Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
-        Me.Refresh()
-        'DrawLine()
-        ' DrawRectangle()
-        'DrawEllipse()
-        DrawString()
-    End Sub
+    '' Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+    'Me.Refresh()
+    '    'DrawLine()
+    '    '  DrawRectangle()
+    '    ' DrawEllipse()
+    '    DrawString()
+    'End Sub
 
     Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove, Me.MouseDown
         Static oldX, oldY As Integer
@@ -91,7 +91,7 @@ Public Class GraphicsExamplesForm
     Private Sub GraphicsExamplesForm_Load(sender As Object, e As EventArgs) Handles Me.Load
     End Sub
 
-    Private Sub ForegroundColorTopMenuItem_Click(sender As Object, e As EventArgs) Handles ForegroundColorTopMenuItem.Click
+    Private Sub ForegroundColorTopMenuItem_Click(sender As Object, e As EventArgs) Handles ForegroundColorTopMenuItem.Click, ForegroundColorContextMenuItem.Click
         Dim result As DialogResult = ColorDialog.ShowDialog()
         If result.ToString = "OK" Then
             ForeGroundColor(ColorDialog.Color)
